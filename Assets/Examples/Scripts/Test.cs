466d99bd70268811bb19d9fa965b2f4a95152f23
sugi.cho <komenari@gmail.com>
2016-09-21 17:20:21 +0900

    no message

--- Assets/Examples/Scripts/Test.cs
+++ Assets/Examples/Scripts/Test.cs
@@ -13,10 +13,13 @@ public class Test : MonoBehaviour
     void Start()
     {
         tex2d = new Texture2D(512, 512, TextureFormat.RGBA32, false);
+        this.Invoke(SomeMethod, 1f);
+        this.CallMethodDelayed(5f, SomeMethod, GetInstanceID(), Time.time, transform.position);
     }
 
     void Update()
     {
+        return;
         var ch = MidiChannel.All;
         Debug.Log(ch);
         var bn = (int)ch;
@@ -25,5 +28,15 @@ public class Test : MonoBehaviour
 
         Debug.Log(RenderTexture.active.format);
         Graphics.CopyTexture(RenderTexture.active, tex2d);
+    }
+
+    void SomeMethod()
+    {
+        print("this is some method");
+    }
+
+    void SomeMethod(int i, float f, Vector3 pos)
+    {
+        Debug.Log(i + ":" + f + ":" + pos);
     }
 }
--- Assets/sugi.cc/Scripts/Extension/Extensions.cs
+++ Assets/sugi.cc/Scripts/Extension/Extensions.cs
@@ -3,163 +3,197 @@ using System.Collections;
 
 namespace sugi.cc
 {
-	static class Extensions
-	{
-		#region MaterialPropertyBlock mpBlock
+    static class Extensions
+    {
+        #region MaterialPropertyBlock mpBlock
 
-		static MaterialPropertyBlock mpBlock {
-			get {
-				if (_mpBlock == null)
-					_mpBlock = new MaterialPropertyBlock ();
-				return _mpBlock;
-			}
-		}
+        static MaterialPropertyBlock mpBlock
+        {
+            get
+            {
+                if (_mpBlock == null)
+                    _mpBlock = new MaterialPropertyBlock();
+                return _mpBlock;
+            }
+        }
 
-		static MaterialPropertyBlock _mpBlock;
+        static MaterialPropertyBlock _mpBlock;
 
-		#endregion
+        #endregion
 
-		public static void Invoke (this MonoBehaviour behaviour, System.Action method, float delay)
-		{
-			behaviour.Invoke (method.Method.Name, delay);
-		}
+        public static void Invoke(this MonoBehaviour behaviour, System.Action method, float time)
+        {
+            behaviour.Invoke(method.Method.Name, time);
+        }
+        public static void InvokeRepeating(this MonoBehaviour behaviour, System.Action method, float time, float repeatRate)
+        {
+            behaviour.InvokeRepeating(method.Method.Name, time, repeatRate);
+        }
+        #region call method with delay by using coroutine
+        static IEnumerator DelayMethodCoroutine(float waitTime, System.Action method)
+        {
+            yield return new WaitForSeconds(waitTime);
+            method();
+        }
+        public static void CallMethodDelayed(this MonoBehaviour behaviour, float time, System.Action method)
+        {
+            behaviour.StartCoroutine(DelayMethodCoroutine(time, method));
+        }
+        public static void CallMethodDelayed<T>(this MonoBehaviour behaviour, float time, System.Action<T> method, T param)
+        {
+            behaviour.StartCoroutine(DelayMethodCoroutine(time, () => { method(param); }));
+        }
+        public static void CallMethodDelayed<T1, T2>(this MonoBehaviour behaviour, float time, System.Action<T1, T2> method, T1 param1, T2 param2)
+        {
+            behaviour.StartCoroutine(DelayMethodCoroutine(time, () => { method(param1, param2); }));
+        }
+        public static void CallMethodDelayed<T1, T2, T3>(this MonoBehaviour behaviour, float time, System.Action<T1, T2, T3> method, T1 param1, T2 param2, T3 param3)
+        {
+            behaviour.StartCoroutine(DelayMethodCoroutine(time, () => { method(param1, param2, param3); }));
+        }
+        public static void CallMethodDelayed<T1, T2, T3, T4>(this MonoBehaviour behaviour, float time, System.Action<T1, T2, T3, T4> method, T1 param1, T2 param2, T3 param3, T4 param4)
+        {
+            behaviour.StartCoroutine(DelayMethodCoroutine(time, () => { method(param1, param2, param3, param4); }));
+        }
+        #endregion
 
-		public static void ApplyGaussianFilter (this RenderTexture s, int nIterations = 3, int lod = 1, RenderTexture d = null)
-		{
-			Gaussian.GaussianFilter (s, d == null ? s : d, nIterations, lod);
-		}
+        public static void ApplyGaussianFilter(this RenderTexture s, int nIterations = 3, int lod = 1, RenderTexture d = null)
+        {
+            Gaussian.GaussianFilter(s, d == null ? s : d, nIterations, lod);
+        }
 
-		/**
+        /**
 		 * use NxN texture
 		 **/
-		public static void DrawTexture (this RenderTexture canvas, Vector2 centerUV, float size, Texture tex, Material drawMat = null)
-		{
-			var pos = new Vector2 (centerUV.x * canvas.width, centerUV.y * canvas.height);
-			size *= canvas.height;
-			var rect = Rect.MinMaxRect (pos.x - size / 2f, pos.y - size / 2f, pos.x + size / 2f, pos.y += size / 2f);
-			var projMat = Matrix4x4.Ortho (0f, canvas.width, 0f, canvas.height, -1f, 1f);
+        public static void DrawTexture(this RenderTexture canvas, Vector2 centerUV, float size, Texture tex, Material drawMat = null)
+        {
+            var pos = new Vector2(centerUV.x * canvas.width, centerUV.y * canvas.height);
+            size *= canvas.height;
+            var rect = Rect.MinMaxRect(pos.x - size / 2f, pos.y - size / 2f, pos.x + size / 2f, pos.y += size / 2f);
+            var projMat = Matrix4x4.Ortho(0f, canvas.width, 0f, canvas.height, -1f, 1f);
 
-			GL.PushMatrix ();
-			GL.LoadIdentity ();
-			GL.LoadProjectionMatrix (projMat);
-			RenderTexture.active = canvas;
-			Graphics.DrawTexture (rect, tex, drawMat);
-			RenderTexture.active = null;
-			GL.PopMatrix ();
-		}
+            GL.PushMatrix();
+            GL.LoadIdentity();
+            GL.LoadProjectionMatrix(projMat);
+            RenderTexture.active = canvas;
+            Graphics.DrawTexture(rect, tex, drawMat);
+            RenderTexture.active = null;
+            GL.PopMatrix();
+        }
 
-		public static void DrawFullscreenQuad (this Material mat, int pass = 0, float z = 1.0f)
-		{
-			if (mat != null)
-				mat.SetPass (pass);
-			GL.Begin (GL.QUADS);
-			GL.Vertex3 (-1.0f, -1.0f, z);
-			GL.Vertex3 (1.0f, -1.0f, z);
-			GL.Vertex3 (1.0f, 1.0f, z);
-			GL.Vertex3 (-1.0f, 1.0f, z);
+        public static void DrawFullscreenQuad(this Material mat, int pass = 0, float z = 1.0f)
+        {
+            if (mat != null)
+                mat.SetPass(pass);
+            GL.Begin(GL.QUADS);
+            GL.Vertex3(-1.0f, -1.0f, z);
+            GL.Vertex3(1.0f, -1.0f, z);
+            GL.Vertex3(1.0f, 1.0f, z);
+            GL.Vertex3(-1.0f, 1.0f, z);
 
-			GL.Vertex3 (-1.0f, 1.0f, z);
-			GL.Vertex3 (1.0f, 1.0f, z);
-			GL.Vertex3 (1.0f, -1.0f, z);
-			GL.Vertex3 (-1.0f, -1.0f, z);
-			GL.End ();
-		}
+            GL.Vertex3(-1.0f, 1.0f, z);
+            GL.Vertex3(1.0f, 1.0f, z);
+            GL.Vertex3(1.0f, -1.0f, z);
+            GL.Vertex3(-1.0f, -1.0f, z);
+            GL.End();
+        }
 
-		public static void DrawFullScreenQuadNxN (this Material mat, int numDivisions = 10, float uvMinX = 0, float uvMinY = 0, float uvMaxX = 1f, float uvMaxY = 1f, int pass = 0, float z = 1.0f)
-		{
-			//上のDrawQuadの、分割数をいじれるバージョン。たくさんオプションがあり、UV付きで、UVの範囲も指定できる（minXY,maxXYの、Rectの範囲のuv）
-			if (mat != null)
-				mat.SetPass (pass);
-			var delta = 1f / numDivisions;
-			GL.Begin (GL.QUADS);
-			for (var y = 0; y < numDivisions; y++)
-				for (int x = 0; x < numDivisions; x++) {
-					var vx0 = 2f * delta * x - 1f;
-					var vy0 = 2f * delta * y - 1f;
-					var tx0 = delta * x;
-					var ty0 = delta * y;
-					GL.MultiTexCoord2 (0, Mathf.Lerp (uvMinX, uvMaxX, tx0), Mathf.Lerp (uvMinY, uvMaxY, ty0));
-					GL.MultiTexCoord2 (1, tx0, ty0);
-					GL.Vertex3 (vx0, vy0, z);
+        public static void DrawFullScreenQuadNxN(this Material mat, int numDivisions = 10, float uvMinX = 0, float uvMinY = 0, float uvMaxX = 1f, float uvMaxY = 1f, int pass = 0, float z = 1.0f)
+        {
+            //上のDrawQuadの、分割数をいじれるバージョン。たくさんオプションがあり、UV付きで、UVの範囲も指定できる（minXY,maxXYの、Rectの範囲のuv）
+            if (mat != null)
+                mat.SetPass(pass);
+            var delta = 1f / numDivisions;
+            GL.Begin(GL.QUADS);
+            for (var y = 0; y < numDivisions; y++)
+                for (int x = 0; x < numDivisions; x++)
+                {
+                    var vx0 = 2f * delta * x - 1f;
+                    var vy0 = 2f * delta * y - 1f;
+                    var tx0 = delta * x;
+                    var ty0 = delta * y;
+                    GL.MultiTexCoord2(0, Mathf.Lerp(uvMinX, uvMaxX, tx0), Mathf.Lerp(uvMinY, uvMaxY, ty0));
+                    GL.MultiTexCoord2(1, tx0, ty0);
+                    GL.Vertex3(vx0, vy0, z);
 
-					GL.MultiTexCoord2 (0, Mathf.Lerp (uvMinX, uvMaxX, tx0 + delta), Mathf.Lerp (uvMinY, uvMaxY, ty0));
-					GL.MultiTexCoord2 (1, tx0 + delta, ty0);
-					GL.Vertex3 (vx0 + 2f * delta, vy0, z);
+                    GL.MultiTexCoord2(0, Mathf.Lerp(uvMinX, uvMaxX, tx0 + delta), Mathf.Lerp(uvMinY, uvMaxY, ty0));
+                    GL.MultiTexCoord2(1, tx0 + delta, ty0);
+                    GL.Vertex3(vx0 + 2f * delta, vy0, z);
 
-					GL.MultiTexCoord2 (0, Mathf.Lerp (uvMinX, uvMaxX, tx0 + delta), Mathf.Lerp (uvMinY, uvMaxY, ty0 + delta));
-					GL.MultiTexCoord2 (1, tx0 + delta, ty0 + delta);
-					GL.Vertex3 (vx0 + 2f * delta, vy0 + 2f * delta, z);
+                    GL.MultiTexCoord2(0, Mathf.Lerp(uvMinX, uvMaxX, tx0 + delta), Mathf.Lerp(uvMinY, uvMaxY, ty0 + delta));
+                    GL.MultiTexCoord2(1, tx0 + delta, ty0 + delta);
+                    GL.Vertex3(vx0 + 2f * delta, vy0 + 2f * delta, z);
 
-					GL.MultiTexCoord2 (0, Mathf.Lerp (uvMinX, uvMaxX, tx0), Mathf.Lerp (uvMinY, uvMaxY, ty0 + delta));
-					GL.MultiTexCoord2 (1, tx0, ty0 + delta);
-					GL.Vertex3 (vx0, vy0 + 2f * delta, z);
-				}
-			GL.End ();
-		}
+                    GL.MultiTexCoord2(0, Mathf.Lerp(uvMinX, uvMaxX, tx0), Mathf.Lerp(uvMinY, uvMaxY, ty0 + delta));
+                    GL.MultiTexCoord2(1, tx0, ty0 + delta);
+                    GL.Vertex3(vx0, vy0 + 2f * delta, z);
+                }
+            GL.End();
+        }
 
-		public static MaterialPropertyBlock GetPropertyBlock (this Renderer renderer)
-		{
-			renderer.GetPropertyBlock (mpBlock);
-			return mpBlock;
-		}
+        public static MaterialPropertyBlock GetPropertyBlock(this Renderer renderer)
+        {
+            renderer.GetPropertyBlock(mpBlock);
+            return mpBlock;
+        }
 
-		#region SetPropertyToRender
+        #region SetPropertyToRender
 
-		public static void SetColor (this Renderer r, string name, Color value)
-		{
-			r.GetPropertyBlock ().SetColor (name, value);
-			r.SetPropertyBlock (mpBlock);
-		}
+        public static void SetColor(this Renderer r, string name, Color value)
+        {
+            r.GetPropertyBlock().SetColor(name, value);
+            r.SetPropertyBlock(mpBlock);
+        }
 
-		public static void SetFloat (this Renderer r, string name, float value)
-		{
-			r.GetPropertyBlock ().SetFloat (name, value);
-			r.SetPropertyBlock (mpBlock);
-		}
+        public static void SetFloat(this Renderer r, string name, float value)
+        {
+            r.GetPropertyBlock().SetFloat(name, value);
+            r.SetPropertyBlock(mpBlock);
+        }
 
-		public static void SetMatrix (this Renderer r, string name, Matrix4x4 value)
-		{
-			r.GetPropertyBlock ().SetMatrix (name, value);
-			r.SetPropertyBlock (mpBlock);
-		}
+        public static void SetMatrix(this Renderer r, string name, Matrix4x4 value)
+        {
+            r.GetPropertyBlock().SetMatrix(name, value);
+            r.SetPropertyBlock(mpBlock);
+        }
 
-		public static void SetTexture (this Renderer r, string name, Texture value)
-		{
-			r.GetPropertyBlock ().SetTexture (name, value);
-			r.SetPropertyBlock (mpBlock);
-		}
+        public static void SetTexture(this Renderer r, string name, Texture value)
+        {
+            r.GetPropertyBlock().SetTexture(name, value);
+            r.SetPropertyBlock(mpBlock);
+        }
 
-		public static void SetVector (this Renderer r, string name, Vector4 value)
-		{
-			r.GetPropertyBlock ().SetVector (name, value);
-			r.SetPropertyBlock (mpBlock);
-		}
+        public static void SetVector(this Renderer r, string name, Vector4 value)
+        {
+            r.GetPropertyBlock().SetVector(name, value);
+            r.SetPropertyBlock(mpBlock);
+        }
 
-		#endregion
+        #endregion
 
-		public static T GetRandom<T> (this T[] array)
-		{
-			return array [array.GetRandomIndex ()];
-		}
+        public static T GetRandom<T>(this T[] array)
+        {
+            return array[array.GetRandomIndex()];
+        }
 
-		public static void Swap<T> (this T[] array)
-		{
-			var tmp = array [0];
-			array [0] = array [1];
-			array [1] = tmp;
-		}
+        public static void Swap<T>(this T[] array)
+        {
+            var tmp = array[0];
+            array[0] = array[1];
+            array[1] = tmp;
+        }
 
-		public static int GetRandomIndex (this System.Array array)
-		{
-			return Random.Range (0, array.Length);
-		}
+        public static int GetRandomIndex(this System.Array array)
+        {
+            return Random.Range(0, array.Length);
+        }
 
-		public static Vector2 GetRandomPoint (this Rect rect)
-		{
-			var x = Random.Range (rect.xMin, rect.xMax);
-			var y = Random.Range (rect.yMin, rect.yMax);
-			return new Vector2 (x, y);
-		}
+        public static Vector2 GetRandomPoint(this Rect rect)
+        {
+            var x = Random.Range(rect.xMin, rect.xMax);
+            var y = Random.Range(rect.yMin, rect.yMax);
+            return new Vector2(x, y);
+        }
 
-	}
+    }
 }
