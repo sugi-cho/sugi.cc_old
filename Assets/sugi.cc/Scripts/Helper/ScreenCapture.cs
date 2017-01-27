13e8c1ce9512f66586e903d97f3e4ac85f7b9a2f
sugi.cho <komenari@gmail.com>
2017-01-27 18:11:57 +0900

    to 5.5.1

--- Assets/sugi.cc/Scripts/Helper/ScreenCapture.cs
+++ Assets/sugi.cc/Scripts/Helper/ScreenCapture.cs
@@ -17,25 +17,40 @@ namespace sugi.cc
                 return _Instance;
             }
         }
-
         static ScreenCapture _Instance;
-        public int fps = 10;
+
+        public int fps = 30;
         public int superSize = 0;
+        public int stopFramecount = 300;
 
         bool recording;
         int framecount;
+        string runTimeName = "";
 
-        // Use this for initialization
-        void Start()
-        {
-            Init();
-        }
+        string fpsStr;
+        string superSizeStr;
+        string stopFramecountStr;
+        bool showGUI;
+
 
         void Init()
         {
             System.IO.Directory.CreateDirectory("Capture");
             Time.captureFramerate = fps;
             framecount = -1;
+            runTimeName = Application.productName + System.DateTime.Now.ToString();
+            runTimeName = runTimeName.Replace('/', '_').Replace(' ', '_').Replace(':', '_').Replace('\\', '_');
+            fpsStr = fps.ToString();
+            superSizeStr = superSize.ToString();
+            stopFramecountStr = stopFramecount.ToString();
+        }
+
+        void Start()
+        {
+            fpsStr = fps.ToString();
+            superSizeStr = superSize.ToString();
+            stopFramecountStr = stopFramecount.ToString();
+            SettingManager.AddExtraGuiFunc(DrawControllGUI);
         }
 
         // Update is called once per frame
@@ -46,11 +61,55 @@ namespace sugi.cc
 
             if (framecount > 0)
             {
-                var name = "Capture/frame" + Time.frameCount.ToString("00000") + ".png";
+                var name = "Capture/" + runTimeName + "_frame" + Time.frameCount.ToString("00000") + ".png";
                 Application.CaptureScreenshot(name, superSize);
             }
             framecount++;
+            if (0 < stopFramecount && stopFramecount < framecount)
+                recording = false;
         }
+
+        void StartRecording()
+        {
+            Init();
+            recording = true;
+            SettingManager.Instance.HideGUI();
+        }
+
+        public static void DrawControllGUI()
+        {
+            Instance.recording = false;
+            GUILayout.BeginVertical("box");
+            if (Instance.showGUI = GUILayout.Toggle(Instance.showGUI, "show ScreenCapture GUI"))
+            {
+                IntField("Capture FPS:", ref Instance.fpsStr, ref Instance.fps);
+                IntField("super size", ref Instance.superSizeStr, ref Instance.superSize);
+                IntField("stop frame count", ref Instance.stopFramecountStr, ref Instance.stopFramecount);
+
+                GUILayout.BeginHorizontal();
+                if (Instance.recording)
+                {
+                    if (GUILayout.Button(string.Format("Stop Recording({0}/{1})", Instance.framecount.ToString("00000"), Instance.stopFramecount.ToString("00000"))))
+                        Instance.recording = false;
+                }
+                else if (GUILayout.Button("Start Recording"))
+                    Instance.StartRecording();
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndVertical();
+        }
+        static void IntField(string label, ref string strVal, ref int targetInt)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(label);
+            strVal = GUILayout.TextField(strVal);
+            GUILayout.EndHorizontal();
+
+            var newInt = targetInt;
+            if (int.TryParse(strVal, out newInt))
+                targetInt = newInt;
+        }
+
 #if UNITY_EDITOR
         [MenuItem("ScreenCapture/Shot")]
         public static void CaptureScreenShot()
@@ -64,24 +123,21 @@ namespace sugi.cc
         public static void StartRecording10()
         {
             Instance.fps = 10;
-            Instance.Init();
-            Instance.recording = true;
+            Instance.StartRecording();
         }
 
         [MenuItem("ScreenCapture/Start/FPS:30")]
         public static void StartRecording30()
         {
             Instance.fps = 30;
-            Instance.Init();
-            Instance.recording = true;
+            Instance.StartRecording();
         }
 
         [MenuItem("ScreenCapture/Start/FPS:60")]
         public static void StartRecording60()
         {
             Instance.fps = 60;
-            Instance.Init();
-            Instance.recording = true;
+            Instance.StartRecording();
         }
 
         [MenuItem("ScreenCapture/Stop")]
